fc7b27c2ce0d81f964625c685ea8278fce769125
Nicolas Perez <nicoperezre@gmail.com>
2023-07-04 11:07:49 -0400

    Stage - First commit

---
+++ stage.php
@@ -0,0 +1,41 @@
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content=""/>
+    <meta name="description" content=""/>
+    <meta property="og:title" content=""/>
+    <meta property="og:description" content=""/>
+
+    <meta property="og:type" content="company"/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content="Banmedica"/>
+    <title>Banmedica</title>
+    <link rel="stylesheet" href="dist/css/style.css?cb=1615307255841">
+</head>
+
+<body class="">
+
+<div class="stage">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="stage__title">
+                    <span>Hola Renato Danilo,</span> <br>
+                    bienvenido nuevamente.
+                </h2>
+                <h3 class="stage__wallet">
+                    Tienes <span>$15.740</span> en excedentes.
+                </h3>
+                <a href="" class="stage__detail">
+                    Revisa el detalle de tus excedentes
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
